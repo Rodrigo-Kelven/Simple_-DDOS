c57fcdb4e65b6b42f4e12b22f7d0b1aaecad644a
Rodrigo_Kelven <92493747+Rodrigo-Kelven@users.noreply.github.com>
2025-01-10 19:54:34 -0300

    Update flood_UDP.py

--- flood_UDP.py
+++ flood_UDP.py
@@ -14,7 +14,7 @@ def enviar_pacotes_udp(ip_destino, porta_destino, numero_pacotes, intervalo):
 if __name__ == "__main__":
     # Configurações do usuário
     ip_destino = input("Digite o IP do destino: ")
-    porta_destino = int(input("Digite a porta do destino: "))
+    porta_destino = int(input("Digite a porta do destino (80,8080,443): "))
     numero_pacotes = int(input("Quantos pacotes deseja enviar? "))
     intervalo = float(input("Intervalo entre os pacotes (em segundos): "))
 
